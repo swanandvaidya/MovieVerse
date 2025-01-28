1ce8ebc1fba211d76b517b9ed1fbdf1957dbc032
Swanand Vaidya <ssvaidya28@gmail.com>
2025-01-28 21:18:46 +0530

    added custom font and updated dependency

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,11 +1,15 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import Colors from '../themes/Colors';
+import Fonts from '../themes/Fonts';
+
+const appIcon = require('../assets/images/app_icon.png');
 
 const SplashScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>SplashScreen</Text>
+      <Image source={appIcon} style={styles.appIcon} />
+      <Text style={styles.welcomeText}>Welcome to MultiVerse</Text>
     </View>
   );
 };
@@ -18,5 +22,15 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.primary,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  appIcon: {
+    width: 100,
+    height: 100,
+  },
+  welcomeText: {
+    color: Colors.white,
+    fontSize: 24,
+    paddingVertical: 30,
+    fontFamily: Fonts.PoppinsRegular,
   },
 });
---
+++ src/themes/Fonts.ts
@@ -0,0 +1,7 @@
+const Fonts = {
+  PoppinsRegular: 'Poppins-Regular',
+  PoppinsBold: 'Poppins-Bold',
+  PoppinsLight: 'Poppins-Light',
+};
+
+export default Fonts;
